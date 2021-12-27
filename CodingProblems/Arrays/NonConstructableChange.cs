d01f5dfbc151d445969af84de8446b06c0fb19f8
Chuck Conway <charles@cconway.com>
2021-12-26 16:57:06 -0800

    Working on the next problem.

---
+++ CodingProblems/Arrays/NonConstructableChange.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CodingProblems.Arrays;
+
+public class NonConstructableChange
+{
+    public void Solution_One()
+    {
+        
+    }
+
+    public int Calculate(int[] coins)
+    {
+        Array.Sort(coins);
+
+        for (int index = 0; index < coins.Length; index++)
+        {
+            
+        }
+    }
+}
