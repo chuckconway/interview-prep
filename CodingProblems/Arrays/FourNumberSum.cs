34a8a315e3fa8d35fcfa1329be71d29965af59b8
Chuck Conway <charles@cconway.com>
2022-01-03 14:49:33 -0800

    In progress...

--- CodingProblems/Arrays/FourNumberSum.cs
+++ CodingProblems/Arrays/FourNumberSum.cs
@@ -23,85 +23,89 @@ public class FourNumberSum
 
         var result = Calculate(array, targetSum);
 
-
         _testOutputHelper.WriteLine(JsonSerializer.Serialize(result));
     }
 
     public List<int[]> Calculate(int[] array, int targetSum)
     {
-        var result = new List<int[]>();
+        var results = new List<int[]>();
 
         if (array.Length > 0)
         {
-            var dictionary = new Dictionary<int, List<int[]>>();
+            var allPairSums = new Dictionary<int, List<int[]>>();
 
             for (var index = 0; index < array.Length; index++)
             {
                 var pointerValue = array[index];
-                
-                for (int innerIndex = 0; innerIndex >= array.Length ; innerIndex++)
+
+                //
+                for (var addToDictionaryIndex = index + 1; addToDictionaryIndex < array.Length; addToDictionaryIndex++)
                 {
+                    
+                }
+
+                for (var innerIndex = 0; innerIndex < array.Length; innerIndex++)
+                {
+                    if (index != innerIndex)
+                    {
                         var innerPointerValue = array[innerIndex];
                         var summedValue = pointerValue + innerPointerValue;
 
+                        AddSumToDictionary(innerIndex, index, dictionary, summedValue, pointerValue, innerPointerValue);
+
                         var missingSum = targetSum - summedValue;
 
-                    if(innerIndex < index)
-                    {
-                        if(dictionary.ContainsKey(missingSum))
-                        {
-                            var summedValues = dictionary[missingSum];
-                            summedValue.Add(new[] {pointerValue, innerPointerValue});
-                        } 
-                        else
-                        {
-                            dictionary.Add(missingSum, new List<int[]>{new []{pointerValue, innerPointerValue}});
-                        }
-                    }
-
-                    if(innerIndex > index && dictionary.ContainsKey(missingSum)) 
-                    {
-
-                    }
-                }
-
-                var firstIndex = index;
-                var secondIndex = index + 1;
-                var leftPointer = secondIndex + 1;
-                var rightPointer = array.Length - 1;
-
-                if (secondIndex >= rightPointer)
-                {
-                    break;
-                }
-                
-                var firstFixedNumberValue = array[firstIndex];
-                var secondFixedNumberValue = array[secondIndex];
-
-                while (leftPointer < rightPointer)
-                {
-                    var leftValue = array[leftPointer];
-                    var rightValue = array[rightPointer];
-
-                    var sum = firstFixedNumberValue + secondFixedNumberValue + leftValue + rightValue;
-
-                    if (sum == targetSum)
-                    {
-                        result.Add(new[] {firstFixedNumberValue, secondFixedNumberValue, leftValue, rightValue});
-                    }
-
-                    if (sum < targetSum)
-                    {
-                        leftPointer++;
-                    }
-                    else
-                    {
-                        rightPointer--;
+                        AddSumsThatEqualTargetSum(innerIndex, index, dictionary, missingSum, pointerValue, innerPointerValue, result);
                     }
                 }
             }
         }
 
         return result;
+    }
+
+    private static void AddSumsThatEqualTargetSum(
+        int innerIndex, 
+        int index, 
+        IReadOnlyDictionary<int, List<int[]>> dictionary, 
+        int missingSum,
+        int pointerValue, 
+        int innerPointerValue, 
+        ICollection<int[]> result)
+    {
+        if (innerIndex > index && dictionary.ContainsKey(missingSum))
+        {
+            var pairs = dictionary[missingSum];
+
+            foreach (var pair in pairs)
+            {
+                var digits = new List<int>();
+                digits.AddRange(new[] {pointerValue, innerPointerValue});
+                digits.AddRange(pair);
+
+                result.Add(digits.ToArray());
+            }
+        }
+    }
+
+    private static void AddSumToDictionary(
+        int innerIndex, 
+        int index, 
+        Dictionary<int, List<int[]>> dictionary, 
+        int summedValue,
+        int pointerValue, int innerPointerValue)
+    {
+        if (innerIndex < index)
+        {
+            if (dictionary.ContainsKey(summedValue))
+            {
+                var summedValues = dictionary[summedValue];
+                summedValues.Add(new[] {pointerValue, innerPointerValue});
+            }
+            else
+            {
+                dictionary.Add(summedValue, new List<int[]> {new[] {pointerValue, innerPointerValue}});
+            }
+        }
     }
 }
