cf09ea74978bf9853d371caa9c88bc76c05a17f3
Chuck Conway <charles@cconway.com>
2022-01-03 05:41:37 +0000

    FourNumberSum in progress

--- CodingProblems/Arrays/FourNumberSum.cs
+++ CodingProblems/Arrays/FourNumberSum.cs
@@ -33,10 +33,38 @@ public class FourNumberSum
 
         if (array.Length > 0)
         {
-            //Array.Sort(array);
+            var dictionary = new Dictionary<int, List<int[]>>();
 
             for (var index = 0; index < array.Length; index++)
             {
+                var pointerValue = array[index];
+                
+                for (int innerIndex = 0; innerIndex >= array.Length ; innerIndex++)
+                {
+                        var innerPointerValue = array[innerIndex];
+                        var summedValue = pointerValue + innerPointerValue;
+
+                        var missingSum = targetSum - summedValue;
+
+                    if(innerIndex < index)
+                    {
+                        if(dictionary.ContainsKey(missingSum))
+                        {
+                            var summedValues = dictionary[missingSum];
+                            summedValue.Add(new[] {pointerValue, innerPointerValue});
+                        } 
+                        else
+                        {
+                            dictionary.Add(missingSum, new List<int[]>{new []{pointerValue, innerPointerValue}});
+                        }
+                    }
+
+                    if(innerIndex > index && dictionary.ContainsKey(missingSum)) 
+                    {
+
+                    }
+                }
+
                 var firstIndex = index;
                 var secondIndex = index + 1;
                 var leftPointer = secondIndex + 1;
