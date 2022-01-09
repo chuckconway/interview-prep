ae751d2ee1f30cf6a2139a8b7f4704cf49e5ff53
Chuck Conway <charles@cconway.com>
2022-01-09 01:34:30 +0000

    Longest Peak in progress

---
+++ CodingProblems/Arrays/Medium/LongestPeak.cs
@@ -0,0 +1,34 @@
+
+
+using Xunit;
+
+namespace CodingProblems.Arrays.Medium;
+
+public class LongestPeak
+{
+    [Fact]
+    public void Calculate()
+    {
+
+    }
+
+    public int Solution_One(int[] array)
+    {
+        bool? increasing = null;
+        bool? decreasing = null;
+        int? previousValue = null;
+        int count = 0;
+        int longest = 0;
+
+        for(var index = 0; index < array.Length; index++)
+        {
+            var value = array[index];
+
+            if(previousValue == null || )
+
+            
+        }
+
+        return -1;
+    }
+}
